7a2e2f7caee39408a16295c6888774282ac68209
AdrianoJoseRibeiro <134211159+AdrianoJoseRibeiro@users.noreply.github.com>
2023-09-20 08:30:15 -0300

    Update desenvolvimentosArrays1.py

--- desenvolvimentosArrays1.py
+++ desenvolvimentosArrays1.py
@@ -5,7 +5,11 @@ O segundo array deve conter os anos de nascimento de familiares e amigos seus.
 Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado 
 para cada lista (strings, booleanos, números inteiros, floats)."""
 lista_lanches = ['x-burguer','x-salada','x-bacon' , 'misto quente','x-egg' ]
+print("Cardapio")
+for lanches in lista_lanches:
+    print(lanches)
 
-
-lista_idade = ['1987','1989',"1990",'1998','2000']
-print(lista_idade,lista_lanches)
+lista_idade = [('Rodrigo',1977),('Eduardo',1980),('Juliana',1989),('Adriano',1990),('Launy',2010)]
+print("Nomes e Anos de Nascimento da familia:")
+for nomes ,ano in lista_idade:
+    print(f"{nomes} = {ano}")
