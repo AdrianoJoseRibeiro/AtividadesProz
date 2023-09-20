05f0fffe64440d7d8f0e453a41796c4c305f8486
AdrianoJoseRibeiro <134211159+AdrianoJoseRibeiro@users.noreply.github.com>
2023-09-20 08:15:29 -0300

    Add files via upload

---
+++ desenvolvimentosArrays1.py
@@ -0,0 +1,11 @@
+"""Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o 
+comando print(). O primeiro array deve conter os produtos de uma loja da 
+sua escolha (loja de comida, materiais de construção, música, etc). 
+O segundo array deve conter os anos de nascimento de familiares e amigos seus. 
+Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado 
+para cada lista (strings, booleanos, números inteiros, floats)."""
+lista_lanches = ['x-burguer','x-salada','x-bacon' , 'misto quente','x-egg' ]
+
+
+lista_idade = ['1987','1989',"1990",'1998','2000']
+print(lista_idade,lista_lanches)
